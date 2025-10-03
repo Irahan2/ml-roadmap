e846ecdef455eab4c0590da7096884648682bed2
Caner <276715@student.pwr.edu.pl>
2025-10-03 20:08:12 +0200

    Problem fixed

--- 12_LangChain/test.py
+++
@@ -1,5 +0,0 @@
-from langserve import RemoteRunnable
-
-chain = RemoteRunnable("http://localhost:8000/chain/c/N4XyA")
-res= chain.invoke({ "language":"Spanish", "text":"Generative AI is a bigger opportunity than Internet" })
-print(res)
